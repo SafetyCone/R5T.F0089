c1009ed3fbaddc77659e82d4bcb593389fa59a65
davidcoats <david@getsafetycone.com>
2022-11-29 23:03:33 -0800

    Initial commit.

---
+++ source/R5T.F0089/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0089
+{
+	/// <summary>
+	/// Code unit context operations (solution context operations, project context operations, etc.).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
